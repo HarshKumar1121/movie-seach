253a1fbf35f7b688e41d9898332a6d460c7382fb
HarshKumar1121 <harshv555erma@gmail.com>
2025-05-07 20:33:30 +0530

    Project Started

---
+++ src/components/navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/globals.css';
+import '../styles/navbar.css';
+
+function navbar() {
+  return (
+    <div>
+        <h1>This is my Nav Bra👙</h1>
+    </div>
+  )
+}
+
+export default navbar
